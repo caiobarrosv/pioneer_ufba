83784143e4eeedbf2aec0f09e5298c66a5be19f7
caiobarrosv <engcaiobarros@gmail.com>
2020-11-02 16:19:30 -0300

    Pure python odometry implemented

--- src/python_odometry.py
+++ src/python_odometry.py
@@ -2,61 +2,51 @@ import numpy as np
 import matplotlib.pyplot as plt
 from math import sin, cos
 
-
-def rot_z(x):
-    """
-    Rotation matrix around the Z axis
-    """
-    rot_z_matrix = np.array([[cos(x), -sin(x), 0],
-                             [sin(x),  cos(x), 0],
-                             [      0,      0, 1]])
-    return rot_z_matrix
-
 def rot_z_simples(x):
     """
     Rotation matrix around the Z axis
+
+    Arguments:
+		x {float} -- Rotation angle
     """
     rot_z_matrix = np.array([[cos(x), -sin(x)],
                              [sin(x),  cos(x)]])
     return rot_z_matrix
 
 def main():
-    
-    
     pose = [0, 0, 0]
     theta = pose[-1] # initial robot orientation
     x = [pose[0]]
     y = [pose[1]]
     
-    L = 0.316
-    r = 0.09
-    dt = 0.1
+    L = 0.316 # Distance between wheels
+    r = 0.09 # Wheel radius
+    dt = 0.1 # Time interval
 
     A = np.array([[r/2, r/2], [-r/L, r/L]])
 
-    vx = 0.5
-    w = 0.1
-    wheel_vel = np.matmul(np.linalg.inv(A), np.array([vx, w]))
-    # essa ordem eh devido ao sinal negativo na segunda linha e primeira coluna da matriz A
-    we = wheel_vel[0] 
-    wd = wheel_vel[1]
+    vx = 0.5 # Linear velocity
+    w = 0.1 # Angular velocity
+
+    wheel_vel = np.matmul(np.linalg.inv(A), np.array([vx, w])) # Wheel velocities
     
-    ve = we * r
-    vd = wd * r
+    we = wheel_vel[0] # Angular velocity of the left wheel
+    wd = wheel_vel[1] # Angular velocity of the right wheel
+    
+    ve = we * r # Linear velocity of the left wheel
+    vd = wd * r # Linear velocity of the right wheel
 
     print('ve: ', ve)
     print('vd: ', vd)
-    raw_input()
-
     for i in np.arange(0, 100, dt):
         if vd == ve:
-            pose = pose + np.array([vx*cos(theta)*dt, vx*sin(theta)*dt, 0])
+            pose = pose + np.array([vx*cos(theta)*dt, vx*sin(theta)*dt, pose[-1]])
             theta = pose[-1]
             x.append(pose[0])
             y.append(pose[1])
         else:
-            R = (L / 2) * ((vd + ve) / (vd - ve))
-            omega = (vd - ve) / L
+            R = (L / 2) * ((vd + ve) / (vd - ve)) # Distance between P and ICC
+            omega = (vd - ve) / L # Angular velocity
             
             ICC_x = pose[0] - R*sin(theta)
             ICC_y = pose[1] + R*cos(theta)
@@ -64,24 +54,19 @@ def main():
 
             rot_z_mat = rot_z_simples(omega*dt)
 
-            ICC_origin = np.array([pose[0] - ICC_x, pose[1] - ICC_y])
+            ICC_origin = np.array([pose[0] - ICC_x, pose[1] - ICC_y]) # ICC at origin
 
             theta = theta + omega*dt
 
-            pose = np.matmul(rot_z_mat, ICC_origin) + ICC
-            
+            pose = np.matmul(rot_z_mat, ICC_origin) + ICC # New pose
             x.append(pose[0])
             y.append(pose[1])
 
     plt.plot(x, y) 
     plt.ylabel('Robot path comparison')
-    plt.ylim(-5, 5)
-    plt.xlim(-5, 5)
+    plt.grid()
+    plt.tight_layout()
     plt.show()
-
 
 if __name__ == "__main__":
     main()
-
-# ICC = pose + np.array([-R*sin(theta), R*cos(theta), omega*dt])
-# ICC_origin = pose - ICC
