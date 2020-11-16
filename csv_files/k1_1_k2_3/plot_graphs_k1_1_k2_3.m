9fbb2fb9159ed355eb0c0bf4752d944862a48c6f
caiobarrosv <engcaiobarros@gmail.com>
2020-11-16 00:34:08 -0300

    Paths for validation included

--- csv_files/k1_1_k2_3/plot_graphs_k1_1_k2_3.m
+++ csv_files/k1_1_k2_3/plot_graphs_k1_1_k2_3.m
@@ -1,25 +1,26 @@
-% pose_1_gt = readtable('ground_truth_path_[0, 5, 0]_k1_1_k2_3.csv', 'HeaderLines', 1);
-% pose_2_gt = readtable('ground_truth_path_[3.54, 3.54, 0]_k1_1_k2_3.csv', 'HeaderLines', 1);
-% pose_3_gt = readtable('ground_truth_path_[5, 0, 0]_k1_1_k2_3.csv', 'HeaderLines', 1);
-% pose_4_gt = readtable('ground_truth_path_[4.5, -2.17, -0.785]_k1_1_k2_3.csv', 'HeaderLines', 1);
-% pose_5_gt = readtable('ground_truth_path_[2.5, -4.33, -1.57]_k1_1_k2_3.csv', 'HeaderLines', 1);
-% pose_6_gt = readtable('ground_truth_path_[0, -5, -1.57]_k1_1_k2_3.csv', 'HeaderLines', 1);
-% pose_7_gt = readtable('ground_truth_path_[-3.54, -3.54, -2.36]_k1_1_k2_3.csv', 'HeaderLines', 1);
-% pose_8_gt = readtable('ground_truth_path_[-5, 0, -3.14]_k1_1_k2_3.csv', 'HeaderLines', 1);
-% pose_9_gt = readtable('ground_truth_path_[-3.54, 3.54, 0]_k1_1_k2_3.csv', 'HeaderLines', 1);
-% 
-% pose_1_odometro = readtable('robot_path_[0, 5, 0]_k1_1_k2_3.csv', 'HeaderLines', 1);
-% pose_2_odometro = readtable('robot_path_[3.54, 3.54, 0]_k1_1_k2_3.csv', 'HeaderLines', 1);
-% pose_3_odometro = readtable('robot_path_[5, 0, 0]_k1_1_k2_3.csv', 'HeaderLines', 1);
-% pose_4_odometro = readtable('robot_path_[4.5, -2.17, -0.785]_k1_1_k2_3.csv', 'HeaderLines', 1);
-% pose_5_odometro = readtable('robot_path_[2.5, -4.33, -1.57]_k1_1_k2_3.csv', 'HeaderLines', 1);
-% pose_6_odometro = readtable('robot_path_[0, -5, -1.57]_k1_1_k2_3.csv', 'HeaderLines', 1);
-% pose_7_odometro = readtable('robot_path_[-3.54, -3.54, -2.36]_k1_1_k2_3.csv', 'HeaderLines', 1);
-% pose_8_odometro = readtable('robot_path_[-5, 0, -3.14]_k1_1_k2_3.csv', 'HeaderLines', 1);
-% pose_9_odometro = readtable('robot_path_[-3.54, 3.54, 0]_k1_1_k2_3.csv', 'HeaderLines', 1);
-
 close all
+% clear all
 clc
+
+pose_1_gt = readtable('ground_truth_path_[0, 5, 0]_k1_1_k2_3.csv', 'HeaderLines', 1);
+pose_2_gt = readtable('ground_truth_path_[3.54, 3.54, 0]_k1_1_k2_3.csv', 'HeaderLines', 1);
+pose_3_gt = readtable('ground_truth_path_[5, 0, 0]_k1_1_k2_3.csv', 'HeaderLines', 1);
+pose_4_gt = readtable('ground_truth_path_[4.5, -2.17, -0.785]_k1_1_k2_3.csv', 'HeaderLines', 1);
+pose_5_gt = readtable('ground_truth_path_[2.5, -4.33, -1.57]_k1_1_k2_3.csv', 'HeaderLines', 1);
+pose_6_gt = readtable('ground_truth_path_[0, -5, -1.57]_k1_1_k2_3.csv', 'HeaderLines', 1);
+pose_7_gt = readtable('ground_truth_path_[-3.54, -3.54, -2.36]_k1_1_k2_3.csv', 'HeaderLines', 1);
+pose_8_gt = readtable('ground_truth_path_[-5, 0, -3.14]_k1_1_k2_3.csv', 'HeaderLines', 1);
+pose_9_gt = readtable('ground_truth_path_[-3.54, 3.54, 0]_k1_1_k2_3.csv', 'HeaderLines', 1);
+
+pose_1_odometro = readtable('robot_path_[0, 5, 0]_k1_1_k2_3.csv', 'HeaderLines', 1);
+pose_2_odometro = readtable('robot_path_[3.54, 3.54, 0]_k1_1_k2_3.csv', 'HeaderLines', 1);
+pose_3_odometro = readtable('robot_path_[5, 0, 0]_k1_1_k2_3.csv', 'HeaderLines', 1);
+pose_4_odometro = readtable('robot_path_[4.5, -2.17, -0.785]_k1_1_k2_3.csv', 'HeaderLines', 1);
+pose_5_odometro = readtable('robot_path_[2.5, -4.33, -1.57]_k1_1_k2_3.csv', 'HeaderLines', 1);
+pose_6_odometro = readtable('robot_path_[0, -5, -1.57]_k1_1_k2_3.csv', 'HeaderLines', 1);
+pose_7_odometro = readtable('robot_path_[-3.54, -3.54, -2.36]_k1_1_k2_3.csv', 'HeaderLines', 1);
+pose_8_odometro = readtable('robot_path_[-5, 0, -3.14]_k1_1_k2_3.csv', 'HeaderLines', 1);
+pose_9_odometro = readtable('robot_path_[-3.54, 3.54, 0]_k1_1_k2_3.csv', 'HeaderLines', 1);
 
 poses = [[0, 5, 0]; [3.54, 3.54, 0]; [5, 0, 0]; [4.5, -2.17, -0.785]; [2.5, -4.33, -1.57]; [0, -5, -1.57];
          [-3.54, -3.54, -2.36]; [-5, 0, -3.14]; [-3.54, 3.54, 0]];
@@ -63,7 +64,28 @@ hold on
 plot(pose_9_gt.Var1, pose_9_gt.Var2, line_color_gt , 'HandleVisibility', 'off')
 
 %% Odometria
-line_color = '--r' 
+poses = [[0, 5, -0.05]; [3.54, 3.54, -0.01]; [5, 0, 0.02]; [4.5, -2.17, -0.785]; [2.5, -4.33, -1.57]; [0, -5, -1.57];
+         [-3.54, -3.54, -2.36]; [-5, 0, -3.14]; [-3.54, 3.54, 0.05]];
+line_color = '--r';
+DrawRobot([pose_1_odometro.Var1(end), pose_1_odometro.Var2(end), poses(1, 3)], line_color )
+hold on
+DrawRobot([pose_2_odometro.Var1(end), pose_2_odometro.Var2(end), poses(2, 3)], line_color )
+hold on
+DrawRobot([pose_3_odometro.Var1(end), pose_3_odometro.Var2(end), poses(3, 3)], line_color )
+hold on
+DrawRobot([pose_4_odometro.Var1(end), pose_4_odometro.Var2(end), poses(4, 3)], line_color )
+hold on
+DrawRobot([pose_5_odometro.Var1(end), pose_5_odometro.Var2(end), poses(5, 3)], line_color )
+hold on
+DrawRobot([pose_6_odometro.Var1(end), pose_6_odometro.Var2(end), poses(6, 3)], line_color )
+hold on
+DrawRobot([pose_7_odometro.Var1(end), pose_7_odometro.Var2(end), poses(7, 3)], line_color )
+hold on
+DrawRobot([pose_8_odometro.Var1(end), pose_8_odometro.Var2(end), poses(8, 3)], line_color )
+hold on
+DrawRobot([pose_9_odometro.Var1(end), pose_9_odometro.Var2(end), poses(9, 3)], line_color )
+hold on
+
 hold on
 plot(pose_1_odometro.Var1, pose_1_odometro.Var2, line_color)
 hold on
--- csv_files/plot_graphs.m
+++ csv_files/plot_graphs.m
@@ -1,10 +1,12 @@
+% clear all
+close all
+clc
+
 % odometry_path = readtable('robot_path_path.csv', 'HeaderLines', 1);
 % ground_truth_path = readtable('ground_truth_path_path.csv', 'HeaderLines', 1);
 % control_states = readtable('control_states_path.csv', 'HeaderLines', 1);
 % robot_velocities = readtable('robot_velocities_path.csv', 'HeaderLines', 1);
 
-close all
-clc
 
 x_odom = odometry_path.Var1;
 y_odom = odometry_path.Var2;
@@ -16,39 +18,43 @@ dist_r = control_states.Var1;
 sigma = control_states.Var2;
 theta = control_states.Var3;
 
-angular_vel = robot_velocities.Var1;
-left_wheel_angular = robot_velocities.Var2;
-linear_vel = robot_velocities.Var3;
-right_wheel_angular = robot_velocities.Var4;
+angular_vel_ground_truth = robot_velocities.Var1;
+angular_vel_odometry = robot_velocities.Var2;
+left_wheel_angular_ground_truth = robot_velocities.Var3;
+left_wheel_angular_odometry = robot_velocities.Var4;
+linear_vel_ground_truth = robot_velocities.Var5;
+linear_vel_odometry = robot_velocities.Var6;
+right_wheel_angular_ground_truth = robot_velocities.Var7;
+right_wheel_angular_odometry = robot_velocities.Var8;
 
-time = robot_velocities.Var5;
+time = robot_velocities.Var9;
 
 %% Posições
 poses = [[1, 1, pi/2]; [3, 1, 0]; [3, -3, -2.36]; [-3, -3, pi/2]; [-3, 0, pi/2]; [0, 0, 0]];
 
-%% Gráfico da velocidade angular e linear
+%% Gráfico da velocidade angular das rodas
 % figure(1)
 % subplot(2, 1, 1)
-% plot(time, linear_vel, 'k')
-% ylabel("Vel. Linear (m/s)")
+% plot(time, left_wheel_angular_ground_truth, 'k')
+% hold on
+% plot(time, left_wheel_angular_odometry, '--r')
+% ylabel("Vel. angular da roda esquerda (rad/s)")
 % xlabel("Tempo (s)")
 % set(gca,'XLim', [0 time(end)+0.2])
-% set(gca, 'XTick', 0 : 5 : time(end)+5)
-% set(gca,'YLim', [min(linear_vel)-0.1 max(linear_vel)+0.1])
-% set(gca, 'YTick', round(min(linear_vel)-0.1, 1) : 0.1 : max(linear_vel)+0.1)
-% hold on
-% 
+% set(gca,'YLim', [min(left_wheel_angular_ground_truth)-0.1 max(left_wheel_angular_ground_truth)+0.1])
+% legend("Ground Truth", "Odometria")
 % grid on
 % 
 % subplot(2, 1, 2)
-% plot(time, angular_vel, 'k')
-% ylabel("Vel. Angular (rad/s)")
+% plot(time, right_wheel_angular_ground_truth, 'k')
+% hold on
+% plot(time, right_wheel_angular_odometry, '--r')
+% ylabel("Vel. angular da roda esquerda (rad/s)")
 % xlabel("Tempo (s)")
 % set(gca,'XLim', [0 time(end)+0.2])
-% set(gca, 'XTick', 0:5:time(end)+5)
-% set(gca,'YLim', [min(angular_vel)-0.1 max(angular_vel)+0.1])
-% set(gca, 'YTick', round(min(angular_vel)-0.1, 1) : 0.2 : max(angular_vel)+0.1)
+% set(gca,'YLim', [min(right_wheel_angular_odometry)-0.1 max(right_wheel_angular_odometry)+0.1])
 % grid on
+% legend("Ground Truth", "Odometria")
 
 %% Gráfico do ground truth VS odometria
 % figure(2)
@@ -64,33 +70,36 @@ poses = [[1, 1, pi/2]; [3, 1, 0]; [3, -3, -2.36]; [-3, -3, pi/2]; [-3, 0, pi/2];
 %     text(poses(i, 1) + 0.1, poses(i, 2) + 0.1, int2str(i))
 % end
 
-%% Gráfico das velocidades angulares das rodas
+%% Gráfico das velocidades angulares do robô
 % figure(3)
 % subplot(2, 1, 1)
-% plot(time, linear_vel, 'k')
+% plot(time, linear_vel_ground_truth, 'k')
+% hold on
+% plot(time, linear_vel_odometry, '--r')
 % ylabel("Vel. Linear (m/s)")
 % xlabel("Tempo (s)")
 % set(gca,'XLim', [0 time(end)+0.2])
 % set(gca, 'XTick', 0 : 5 : time(end)+5)
-% set(gca,'YLim', [min(linear_vel)-0.1 max(linear_vel)+0.1])
-% set(gca, 'YTick', round(min(linear_vel)-0.1, 1) : 0.1 : max(linear_vel)+0.1)
-% hold on
-% 
+% set(gca,'YLim', [min(linear_vel_ground_truth)-0.05 max(linear_vel_ground_truth)+0.1])
+% set(gca, 'YTick', round(min(linear_vel_ground_truth)-0.05, 1) : 0.1 : max(linear_vel_ground_truth)+0.1)
+% legend("Ground Truth", "Odometria")
 % grid on
 % 
 % subplot(2, 1, 2)
-% plot(time, angular_vel, 'k')
+% plot(time, angular_vel_ground_truth, 'k')
+% hold on
+% plot(time, angular_vel_odometry, '--r')
 % ylabel("Vel. Angular (rad/s)")
 % xlabel("Tempo (s)")
 % set(gca,'XLim', [0 time(end)+0.2])
 % set(gca, 'XTick', 0:5:time(end)+5)
-% set(gca,'YLim', [min(angular_vel)-0.1 max(angular_vel)+0.1])
-% set(gca, 'YTick', round(min(angular_vel)-0.1, 1) : 0.2 : max(angular_vel)+0.1)
+% set(gca,'YLim', [min(angular_vel_ground_truth)-0.1 max(angular_vel_ground_truth)+0.1])
+% set(gca, 'YTick', round(min(angular_vel_ground_truth)-0.1, 1) : 0.2 : max(angular_vel_ground_truth)+0.1)
+% legend("Ground Truth", "Odometria")
 % grid on
 
 %% Control states
 % figure(4)
-% 
 % subplot(3, 1, 1)
 % plot(time, sigma, 'k')
 % ylabel("\delta (rad)")
